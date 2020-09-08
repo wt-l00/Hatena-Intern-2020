8eb5a9a0c33c0b0c18f160bc9d98e9ccbc3350ba
wt-l00 <ei13suke@gmail.com>
2020-09-08 10:50:08 +0900

    Add goldmark
    h1 tagが効かない

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -3,23 +3,15 @@ package renderer
 import (
 	"bytes"
 	"context"
-	"html/template"
-	"regexp"
-)
 
-var urlRE = regexp.MustCompile(`https?://[^\s]+`)
-var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
+	"github.com/yuin/goldmark"
+)
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
-	// TODO: これはサンプル実装 (URL の自動リンク) です
-	html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-		var w bytes.Buffer
-		err := linkTmpl.ExecuteTemplate(&w, "link", url)
-		if err != nil {
-			return url
-		}
-		return w.String()
-	})
-	return html, nil
+	var buf bytes.Buffer
+	if err := goldmark.Convert([]byte(src), &buf); err != nil {
+		panic(err)
+	}
+	return buf.String(), nil
 }
