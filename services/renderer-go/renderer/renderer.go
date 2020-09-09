a2b50f2b422fb8143f6107ed508e6c35a566263f
wt-l00 <ei13suke@gmail.com>
2020-09-09 18:34:06 +0900

    Update renderer logic

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -4,13 +4,23 @@ import (
 	"bytes"
 	"context"
 
+	pb_fetcher "github.com/wt-l00/Hatena-Intern-2020/services/renderer-go/pb/fetcher"
 	commentout "github.com/wt-l00/goldmark-commentout"
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/text"
+	"github.com/yuin/goldmark/util"
 )
 
+type autoTitleLinker struct {
+	ctx        context.Context
+	fetcherCli pb_fetcher.FetcherClient
+}
+
 // Render は受け取った文書をHTMLとして返す
-func Render(ctx context.Context, src string) (string, error) {
-	html, err := ConvertMd(src)
+func Render(ctx context.Context, src string, fetcherClient pb_fetcher.FetcherClient) (string, error) {
+	html, err := ConvertMd(ctx, src, fetcherClient)
 	if err != nil {
 		return "", err
 	}
@@ -18,9 +28,14 @@ func Render(ctx context.Context, src string) (string, error) {
 }
 
 // ConvertMd は受け取った文書（markdown）を HTMLに変換する
-func ConvertMd(src string) (string, error) {
+func ConvertMd(ctx context.Context, src string, fetcherCli pb_fetcher.FetcherClient) (string, error) {
 	var buf bytes.Buffer
 	var md = goldmark.New(
+		goldmark.WithParserOptions(
+			parser.WithASTTransformers(
+				util.Prioritized(&autoTitleLinker{fetcherCli: fetcherCli, ctx: ctx}, 999),
+			),
+		),
 		goldmark.WithExtensions(
 			commentout.Commentout,
 		),
@@ -30,4 +45,23 @@ func ConvertMd(src string) (string, error) {
 	}
 
 	return buf.String(), nil
+}
+
+func (l *autoTitleLinker) Transform(node *ast.Document, reader text.Reader, pc parser.Context) {
+	ast.Walk(node, func(node ast.Node, entering bool) (ast.WalkStatus, error) {
+		if node, ok := node.(*ast.Link); ok && entering && node.ChildCount() == 0 {
+			node.AppendChild(node,
+				ast.NewString([]byte(fetchTitle(l.ctx, l.fetcherCli, string(node.Destination)))))
+		}
+		return ast.WalkContinue, nil
+	})
+}
+
+// fetchTitle は FetcherClient を使用
+func fetchTitle(ctx context.Context, fetcherCli pb_fetcher.FetcherClient, url string) string {
+	reply, err := fetcherCli.Fetch(ctx, &pb_fetcher.FetcherRequest{Src: url})
+	if err != nil {
+		return ""
+	}
+	return reply.Title
 }
--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -5,9 +5,27 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	pb_fetcher "github.com/wt-l00/Hatena-Intern-2020/services/renderer-go/pb/fetcher"
+	"google.golang.org/grpc"
 )
 
+type fetcherClientInterface interface {
+	Fetch(ctx context.Context, in *pb_fetcher.FetcherRequest, opts ...grpc.CallOption) (*pb_fetcher.FetcherReply, error)
+}
+
+type mockedFetcherClient struct {
+	returnValue string
+}
+
+func (m *mockedFetcherClient) Fetch(ctx context.Context, in *pb_fetcher.FetcherRequest, opts ...grpc.CallOption) (*pb_fetcher.FetcherReply, error) {
+	return &pb_fetcher.FetcherReply{Title: m.returnValue}, nil
+}
+
 func Test_Render_Htag(t *testing.T) {
+	fetcherCli := &mockedFetcherClient{
+		returnValue: "sample",
+	}
+
 	htagTests := []struct {
 		actual   string
 		expected string
@@ -21,25 +39,31 @@ func Test_Render_Htag(t *testing.T) {
 	}
 
 	for _, htag := range htagTests {
-		html, err := Render(context.Background(), htag.actual)
+		html, err := Render(context.Background(), htag.actual, fetcherCli)
 		assert.NoError(t, err)
 		assert.Equal(t, htag.expected, html)
 	}
 }
 
 func Test_Render_Link(t *testing.T) {
+	fetcherCli := &mockedFetcherClient{
+		returnValue: "sample",
+	}
 	src := `[Google](https://google.com)`
-	html, err := Render(context.Background(), src)
+	html, err := Render(context.Background(), src, fetcherCli)
 	assert.NoError(t, err)
 	assert.Equal(t, "<p><a href=\"https://google.com\">Google</a></p>\n", html)
 }
 
 func Test_Render_List(t *testing.T) {
+	fetcherCli := &mockedFetcherClient{
+		returnValue: "sample",
+	}
 	src := `
 - list1
 - list2
 - list3`
-	html, err := Render(context.Background(), src)
+	html, err := Render(context.Background(), src, fetcherCli)
 	assert.NoError(t, err)
 	assert.Equal(t, `<ul>
 <li>list1</li>
@@ -50,8 +74,21 @@ func Test_Render_List(t *testing.T) {
 }
 
 func Test_Render_Commentout(t *testing.T) {
+	fetcherCli := &mockedFetcherClient{
+		returnValue: "Example Domain",
+	}
 	src := `//TODO: something//`
-	html, err := Render(context.Background(), src)
+	html, err := Render(context.Background(), src, fetcherCli)
 	assert.NoError(t, err)
 	assert.Equal(t, "<!-- TODO: something -->", html)
+}
+
+func Test_Render_Autolink(t *testing.T) {
+	fetcherCli := &mockedFetcherClient{
+		returnValue: "Example Domain",
+	}
+	src := `[](https://example.com)`
+	html, err := Render(context.Background(), src, fetcherCli)
+	assert.NoError(t, err)
+	assert.Equal(t, "<p><a href=\"https://example.com\">Example Domain</a></p>\n", html)
 }
